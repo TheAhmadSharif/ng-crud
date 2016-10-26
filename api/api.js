a1baa46b1c1f6fbb01ea91e5373b0e10d76996de
Ahmad <ahmadshorif@gmail.com>
2016-10-26 16:36:48 +0600

    api | service | factory

--- api/api.js
+++ api/api.js
@@ -12,7 +12,8 @@ var socket = require("socket.io")(server);
 appLisent.use(bodyParser.json());
 appLisent.use(bodyParser.urlencoded({ extended: true }));
 
-appLisent.use(cors())
+
+appLisent.use(cors());
 
 /* Mongo Parameters */
 var mongo = require("mongodb")
@@ -23,116 +24,54 @@ var db = new mongo.Db("crud", new mongo.Server(host, port, {}));
 db.open();
 /* JSON API */
 
-appLisent.get('/api', MONGO_SELECT)
-appLisent.get('/api/:id', MONGO_SELECT_ID)
-appLisent.post('/api', MONGO_INSERT)
-appLisent.put('/api/:id', MONGO_UPDATE_ID)
-appLisent.delete('/api/:id', MONGO_DELETE_ID)
-appLisent.get('/api/check/:firstname', MONGO_UNIQUE_CODE )
+appLisent.get('/api', MongoFind);
+appLisent.post('/api', MongoInsert);
+appLisent.delete('/api/:id', MongoRemove);
 
-
-
-/* Delete ID */
-
-function MONGO_DELETE_ID(req, res){
-    var id = req.params.id;  
-    var obj_id = new ObjectID(id);  
-  
-    db.collection('crud', function(err, collection) {
-        collection.remove({'_id': obj_id } ,function(err, i) {
-            res.end(JSON.stringify({'message':'Delete Succesfull'}));    //get all data from db and render on index page 
-        });
-    });
-}
 
 
  
 /* Mongo Get */
-
-function MONGO_SELECT(req, res) {
-	db.collection('crud', function (err, collection){
-		collection.find().toArray(function(err, result){
-			res.end(JSON.stringify(result))
+function MongoFind(httpRequest, httpResponse) {
+	db.collection('Student', function (error, collection){
+		collection.find().toArray(function(error, result){
+            httpResponse.send(JSON.stringify(result));
 		})
 	})
 }
 
-function MONGO_INSERT(req, res) {
-	var data = req.body;
 
-	db.collection('crud', function(err,collection) {
-		collection.insert(data, function(err, result) {
-			if (err) {
-				res.end(JSON.stringify(
-					{'error': 'An error has occured'}));
-
-				res.end(JSON.stringify({'status':'Save Succesfull'}));       
-			}
-
-            res.send(result);
-            socket.emit('speak', 'One Date Inserted');
-		})
-	})
+/* Insert */
+function MongoInsert (httpRequest, httpResponse) {
+    console.log("Insert data");
+    var data = httpRequest.body;
+    db.collection('Student', function (error, collection) {
+        collection.insert(data, function (error, result) {
+            console.log(result, 'error');
+            if (error) {
+                httpResponse.end(JSON.stringify({'error': 'Error occured'}))
+            }
+           
+            httpResponse.send(result);
+            socket.emit('speak', 'One Data Inserted');
+        })
+    })
 }
-
-function MONGO_UNIQUE_CODE(req, res){
-    var firstname = req.params.firstname;   
-  
-    db.collection('crud', function(err, collection) {
-        collection.find({'firstname':firstname}).toArray(function(err,result) {
-            if(result){
-            res.end(JSON.stringify(result))    //get all data from db  
+function MongoRemove (httpRequest, httpResponse) {
+    console.log(httpResponse);
+    db.collection('Student', function (error, collection ) {
+        collection.remove({ '_id': ObjectID, function (error, result) {
+                if (error) {
+                    httpResponse.end(json.stringify({'error': 'Error Occured'}))
+                }
+                httpResponse.send(JSON.stringify(result));
+            } 
+        })
+    })    
 }
-            
-        });
-    });
-}
-
-
-function MONGO_SELECT_ID(req, res){
-    var id = req.params.id;
-    var obj_id = new ObjectID(id);    
-    console.log(id);
-   
-    db.collection('crud', function(err, collection) {
-        collection.find({'_id':obj_id}).toArray(function(err,result) {
-            
-            res.end(JSON.stringify(result))    //get all data from db  
-        });
-    });
-
-
-}
-
-function MONGO_UPDATE_ID(req, res){
-    
-    var id = req.params.id;
-	var obj_id = new ObjectID(id); 
-    
-    var body=req.body;
-
-    console.log(req.body)
-    var input = JSON.parse(JSON.stringify(req.body))
-    console.log(input.name);
-
-            db.collection('crud', function(err, collection) {
-                collection.update({'_id':obj_id},body,function(err, result) {
-                    if (err) {
-                            res.end(JSON.stringify({'status':'Update not done'})); 
-                    } else {
-                        console.log('' + result + ' document(s) updated');                  
-                        collection.findOne({'_id':obj_id}, function(err, item) {
-                            
-                             res.end(JSON.stringify({'updated data':item}));            
-                        });                 
-                    }
-                });
-            });
-}
-
 
 
 function connectionStatus(){
-console.log("Listening on port localhost:3300/api")
+    console.log("Listening on port localhost:3300/api")
 }
 server.listen(3300,connectionStatus)
--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 
 var app = angular.module('crudApp',['ngResource']);
-app.controller('crudController', ['$scope', '$http','$resource', 'crudFactory',function($scope, $http, $resource, crudFactory) {
 
-
+app.controller('crudController', ['$scope', '$http','$resource','serviceID',function($scope, $http, $resource, serviceID) {
 
   $scope.initState = function () {
       $scope.student = {};
@@ -11,132 +10,88 @@ app.controller('crudController', ['$scope', '$http','$resource', 'crudFactory',f
       $scope.student.updateBtn = false;
   }
 
-  $scope.initState();
-
-  $scope.EditData = function () {
-    $scope.EditBlock= true; 
-  }
-
-  /* GET DATA FROM DB */
-  $scope.get = function get() {
-    crudFactory.get('',{},function(data){
-      $scope.items = data;
-      
+  serviceID.square(2);
+  var updateData = function () {
+    serviceID.getData(function(hello){
+        $scope.student = hello.data;
     });
   }
 
-$scope.get();
+  serviceID.postData(updateData);
 
-
-
-
-$scope.addData = function () {
-
-var postData = {
-	first_name: $scope.student.firstname,
-	last_name: $scope.student.lastname,
-}
-
-if ($scope.student.firstname != null && $scope.student.lastname != null) {
-    crudFactory.post(postData).$promise.then(function(data) {
-      $scope.get();
-
-    }, function(error) {
-      console.log("Save not done,please try again");
-    });
-}
-
-
-  $scope.initState();
-}
-
-
-
-
-$scope.editData = function (data) {
-  console.log(data);
-  $scope.student = {};
-  $scope.student.firstname = data.first_name; 
-  $scope.student.lastname = data.last_name;
-  $scope.student.addType = 'Edit';
-  $scope.student.addBtn = false;
-  $scope.student.updateBtn = true;
-}
-
-  
-
-
-
-
-$scope.DeleteData = function (id) {
-  var id = id;
- 
-  if (id) {
-      crudFactory.delete('http://localhost:3300/api/:id', {id: id }, function (data){
-            $scope.get();
-     });
-      
+  $scope.removeObject = function (id, updateData) {
+    console.log(id);
+    serviceID.remove(id)
   }
-}
 
 
+}])
 
-}]);
-
-
-
-app.directive('preventDefault', [function(){
-	return {
-		restrict: 'ACE',
-		link: function(scope, element, attrs) {
-				 element.on('click', function(e){
-                    e.preventDefault();
-                    e.stopPropagation()
-
-                });
-		}
-	}
-}]);
-
-app.factory('crudFactory', function($resource) {
-        return $resource(
-          'http://localhost:3300/api/:id', 
-          {
-                id: '@id'
-           },
-           {
-                post: {
-                        method: 'POST'
-                },
-                update: {
-                        method: 'PUT'
-                },
-                get: {
-                        method: 'GET',
-                        isArray: true
-                },
-                delete: {
-                        method: 'DELETE'
-                }
-        });
+app.directive('preventDefault', function () {
+    return {
+      link: function (scope, elem) {
+        elem.on('click', function (event){
+          event.preventDefault();
+        })
+      }
+    }
 });
 
+app.service('serviceID', ['$http', function ($http) {
+this.square = function (x) {
+  return x * x;
+}
 
-app.service('TestServices', ['$http', function($http) {
-  this.value = '';  
-  this.get = function() {
-    return $http.get('http://localhost:3300/api');
-  };
-  this.post = function() {
-    return $http.post('http://localhost:3300/api/', 'x');
+this.getData = function (callback) {
+  $http({
+    method: 'GET',
+    url: 'http://localhost:3300/api'
+  }).then(function successCallback (response) {
+    callback(response);
+  }, 
+    function errorCallback (error) {
+      console.log(error);
+    }
+  );
+}
+
+
+this.postData = function (updateData) {
+  $http({
+    method: 'POST', 
+    url: 'http://localhost:3300/api',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    data: {
+      firstname: 'Ahmad',
+      lastname: 'Sharif'
+    }
+
+    }).then(function successCallback(response) {
+      updateData();
+    }, function failureCallback (error) {
+        console.log(error);
+    })
   }
 
-  this.ahmad = function (data) {
-    this.value = data;
-  }
-
-  this.getDAta = function(){
-    return this.value;
-  }
+this.remove = function (ObjectId, updateData) {
+  $http({
+    method: 'DELETE',
+    url: 'http://localhost:3300/api/:id',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    data: {
+      id: ObjectId
+    }
+    
+  }).then (function successCallback (httpResponse) {
+    updateData()
+  }, function failureCallback (failureCallback) {
+     console.log(failureCallback);
+  })
+}
 
 }]);
+
